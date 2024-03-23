fb75ccda3abd2d2ffe3debb8e52ce1447d5dd9f6
nika-lomiashvili <mr.lomiashvili@mail.ru>
2024-03-23 12:09:15 +0400

    update admin

--- content/admin.py
+++ content/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Category, Tag, Article
 # Register your models here.
 
-admin.site.register(Category, Tag, Article)
+
+admin.site.register(Tag)
+admin.site.register(Article)
--- modules/admin.py
+++ modules/admin.py
@@ -2,4 +2,5 @@ from django.contrib import admin
 from .models import Block, Category
 # Register your models here.
 
-admin.site.register(Block, Category)
+admin.site.register(Block)
+admin.site.register(Category)
